378583bad5d5a24707a71655c7ca4ca462356804
McCarthySonrai <jason.mccarthy@sonraisecurity.com>
2020-12-22 14:55:46 -0400

    fix

--- bot001/bot.py
+++ bot001/bot.py
@@ -8,7 +8,7 @@ import json
 def run(ctx):
     # Get data for bot from ctx
     resource_id = sonrai.platform.aws.arn.parse(ctx.resource_id)
-
+    
     # Get EC2 instance id from resource id
     instance_id = resource_id \
       .assert_service("ec2") \
@@ -21,10 +21,7 @@ def run(ctx):
       .region
         
     # Create AWS identity and access management client
-    ec2_client = boto3.client('ec2',region)
-    ec2_resource = boto3.resource('ec2',region)
-    sts_client = boto3.client('sts')
-    logging.info(sts_client.get_caller_identity())
+    ec2_client = ctx.get_client('ec2',region)
 
     # Verify instance exists
     logging.info("Checking for instance in region: {}".format(region))
@@ -51,15 +48,20 @@ def run(ctx):
         group_id = new_group['GroupId'] 
         logging.info("Created sonrai-deny-all security group with id: {}".format(group_id))
 
-    # Make absolutely certain that the deny-all group has no ingress/egress rules
-    sg = ec2_resource.SecurityGroup(group_id)
-    if sg.ip_permissions:
-      sg.revoke_ingress(IpPermissions=sg.ip_permissions)
-    if sg.ip_permissions_egress:
-      sg.revoke_egress(IpPermissions=sg.ip_permissions_egress)
+    # Make absolutely certain that the deny-all group has no ingress/egress rules.  If this rule was created just above, it will have a defaul allow-all egress rule.
+    sg_details = ec2_client.describe_security_groups(GroupIds=[group_id])
+
+    if sg_details["SecurityGroups"][0]["IpPermissionsEgress"]:
+      logging.info("Truncating egress rules for sonrai-deny-all")
+      logging.info("Truncating egress rules for sonrai-deny-all")
+      ec2_client.revoke_security_group_egress(GroupId=group_id,IpPermissions=sg_details["SecurityGroups"][0]["IpPermissionsEgress"])
+    if sg_details["SecurityGroups"][0]["IpPermissions"]:
+      logging.info("Truncating ingress rules for sonrai-deny-all")
+      ec2_client.revoke_security_group_ingress(GroupId=group_id,IpPermissions=sg_details["SecurityGroups"][0]["IpPermissions"])
 
     # Iterate through network interfaces, and remove current security groups from EC2 instance by specifying the group list, including only the new deny-all group.
     for network_interface in  instance_details["Reservations"][0]["Instances"][0]["NetworkInterfaces"]:
       logging.info("Assigning deny-all security group to ENI with id: {}".format(network_interface["NetworkInterfaceId"]))
       response = ec2_client.modify_network_interface_attribute(NetworkInterfaceId=network_interface["NetworkInterfaceId"],Groups=[group_id]) 
+
 
